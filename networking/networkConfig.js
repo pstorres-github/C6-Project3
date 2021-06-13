4848bd71ecfa865f9ece8d8ede20f2a1935ccdbe
araadt <andy@vanderraadt.codes>
2021-06-12 19:13:38 -0600

    init

---
+++ networking/networkConfig.js
@@ -0,0 +1,15 @@
+const droneIP = '192.168.10.1'
+const localListenerIP = '0.0.0.0'
+const commandPORT = 8889
+const statePORT = 8890
+const videoPORT = 11111
+
+export { droneIP, localListenerIP, commandPORT, statePORT, videoPORT }
+
+// module.exports = {
+//     droneIP: droneIP,
+//     localListenerIP: localListenerIP,
+//     commandPORT: commandPORT,
+//     statePORT: statePORT,
+//     videoPORT: videoPORT,
+// }
