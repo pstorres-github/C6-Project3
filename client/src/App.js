d30c7e36f2005c0ebf80cc90b9e19c0d9ebdc948
araadt <andy@vanderraadt.codes>
2021-06-22 09:55:23 -0600

    rename to coordinate with plan

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='app-content'>
+       DRONE-APP CONTENT GOES HERE
+      </div>
+    </div>
+  )
+}
+
+export default App;
