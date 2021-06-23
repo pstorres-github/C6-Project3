d725fca0a3895f043be88c6b334e86df83e69507
mKhan-11 <80430158+mKhan-11@users.noreply.github.com>
2021-06-22 19:12:07 -0600

    Updated README file, renamed client folder to pilotconsole, mvarious minor fixes

---
+++ pilotconsole/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='app-content'>
+        PILOT CONSOLE FOR DRONE-APP GOES HERE
+      </div>
+    </div>
+  )
+}
+
+export default App;
