f5e1fddef99ed619820f2d9b8c6985475a2e7d04
mKhan-11 <80430158+mKhan-11@users.noreply.github.com>
2021-06-25 00:09:23 -0600

    added DroneStatus & SendCommand components

---
+++ pilotconsole/src/components/DroneStatus.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react"
+
+const DroneStatus = ({ DroneConnection }) => {
+
+    const [connectionStatus, setConnectionstatus] = useState('Disconnected')
+
+    const droneIP = '192.168.10.1'
+    const commandPORT = 8889
+
+    const droneStartup = (droneCommand) => {
+
+        DroneConnection.send(droneCommand, 0, droneCommand.length, commandPORT, droneIP, (error, bytes) => {
+            if (error) throw error
+            else {
+                console.log('Command : ' + droneCommand)
+            }
+        })
+
+    }
+
+    //Initial Connection
+    useEffect(() => {
+        droneStartup('command')
+
+        DroneConnection.on('message', (msg, info) => {
+            setConnectionstatus(msg.toString())
+            console.log('Data Received From Drone : ' + msg.toString())
+        })
+
+    }, [])
+
+    return (
+        <div>Drone Status: {connectionStatus} </div>
+    )
+}
+
+export default DroneStatus
---
+++ pilotconsole/src/components/SendCommand.js
@@ -0,0 +1,41 @@
+import DroneConnection from '../App'
+
+export function socketHandling() {
+
+    /// Drone Socket Connection ///
+
+    // Error Handling
+    DroneConnection.on('error', (error, info) => {
+        console.log('Error Received From Drone : ' + error.toString())
+        // DroneConnection.close()
+    })
+
+    // Connection & Listening
+    DroneConnection.on('listening', () => {
+        const address = DroneConnection.address()
+        console.log(`Drone Connected & Listening on : ${address.address}:${address.port}`)
+    })
+
+    // Message Handling
+    DroneConnection.on('message', (msg, info) => {
+        console.log('Data Received From Drone : ' + msg.toString())
+    })
+
+}
+
+// Send Command to Drone
+const sendCommand = () => {
+    const droneCommand = 'command'
+    const droneIP = '192.168.10.1'
+    const commandPORT = 8889
+
+    DroneConnection.send(droneCommand, 0, droneCommand.length, commandPORT, droneIP, (error, bytes) => {
+        if (error) throw error
+        else {
+            console.log('Command : ' + droneCommand)
+        }
+    })
+
+}
+
+export default sendCommand
