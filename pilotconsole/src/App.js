caddda60beba8726d3f2d3d3eb2aab9b466ad6bc
mKhan-11 <80430158+mKhan-11@users.noreply.github.com>
2021-06-25 00:12:50 -0600

    cleaned up index.js lint, added socket connection to App.js

--- pilotconsole/src/App.js
+++ pilotconsole/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
-
-import { Switch, Route, BrowserRouter as Router} from 'react-router-dom'
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 
 import './App.css'
 
@@ -8,26 +7,32 @@ import Homepage from './pages/Homepage'
 import PilotConsole from './pages/PilotConsole'
 import AuthenticationProvider from './AuthenticationProvider'
 
+const electron = window.require('electron')
+const dgram = electron.remote.require('dgram')
+
+const DroneConnection = dgram.createSocket('udp4')
+DroneConnection.bind(8001)
 
 function App() {
 
   return (
     <AuthenticationProvider>
-    
-    <Router>
-    
-      <div className='app-content'>
-            <Switch>
-              <Route exact path='/' render={() => (<Homepage />)} />
-              <Route exact path='/pilotconsole' render={() => (<PilotConsole />)} />
-            </Switch>
-      </div>
 
-    </Router>
+      <Router>
+
+        <div className='app-content'>
+          <Switch>
+            <Route exact path='/' render={() => (<Homepage />)} />
+            <Route exact path='/pilotconsole' render={() => (<PilotConsole DroneConnection={DroneConnection} />)} />
+          </Switch>
+        </div>
+
+      </Router>
     </AuthenticationProvider>
 
 
   )
 }
 
+export {DroneConnection}
 export default App;
--- pilotconsole/src/components/console/DroneStatus.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const DroneStatus = () => {
-    return (<div>Drone Status</div>)
-}
-
-export default DroneStatus
