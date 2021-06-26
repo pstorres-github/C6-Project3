5410cedea3ae4da994ed58ae64aa3e451e184058
mKhan-11 <80430158+mKhan-11@users.noreply.github.com>
2021-06-26 02:53:12 -0600

    added loadscreen

--- pilotconsole/public/main.js
+++ pilotconsole/public/main.js
@@ -1,28 +1,50 @@
 const { app, BrowserWindow } = require('electron')
 require('@electron/remote/main').initialize()
 
-function createWindow() {
-    // Define the Pilot Console Window Parameters
-    const pilotConsoleWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            enableRemoteModule: true
-        }
-    })
-
-    // Load Window from React App's Location
-    pilotConsoleWindow.loadURL('http://localhost:3000')
-
-}
-
 // Name the Electron Application
 app.setName('Pilot Console')
 
-// Create the Pilot Console Browser Window
-app.on('ready', createWindow)
+app.on('ready', () => {
+    const loadScreen = new BrowserWindow({
+        show: false,
+        frame: false,
+        width: 600,
+        height: 300,
+        center: true,
+        backgroundColor: '#272727'
+
+    })
+
+    loadScreen.once('show', () => {
+
+        // Define the Pilot Console Window Parameters
+        const pilotConsoleWindow = new BrowserWindow({
+            show: false,
+            width: 1366,
+            height: 768,
+            center: true,
+            webPreferences: {
+                nodeIntegration: true,
+                contextIsolation: false,
+                enableRemoteModule: true
+            }
+        })
+
+        pilotConsoleWindow.webContents.once('ready-to-show', () => {
+            // Allow App to Take Up All of the Available Screen <-- Uncomment the next line once development is complete ////
+            // pilotConsoleWindow.maximize()
+            pilotConsoleWindow.show()
+            pilotConsoleWindow.focus()
+            loadScreen.hide()
+            loadScreen.close()
+        })
+        // Load Window from React App's Location
+        pilotConsoleWindow.loadURL('http://localhost:3000')
+    })
+    // loadScreen.loadFile('loading.html')
+    loadScreen.loadURL(`file://${__dirname}/loadscreen.html`)
+    loadScreen.show()
+})
 
 
 // MacOS Event Listeners - Quit when all Windows Are Closed
@@ -39,6 +61,22 @@ app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
+        const pilotConsoleWindow = new BrowserWindow({
+            width: 1366,
+            height: 768,
+            center: true,
+            webPreferences: {
+                nodeIntegration: true,
+                contextIsolation: false,
+                enableRemoteModule: true
+            }
+        })
+
+        // Load Window from React App's Location
+        pilotConsoleWindow.loadURL('http://localhost:3000')
+
+        // Allow App to Take Up All of the Available Screen <-- Uncomment the next line once development is complete ////
+        // pilotConsoleWindow.maximize()
+        pilotConsoleWindow.focus()
     }
 })
