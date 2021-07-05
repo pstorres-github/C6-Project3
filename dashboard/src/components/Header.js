f3aff5b27658e76064931eed21533cf429784485
PTorres <pstorres@gmail.com>
2021-07-04 21:54:26 -0600

    added header bar for logout button

---
+++ dashboard/src/components/Header.js
@@ -0,0 +1,23 @@
+import AuthenticationContext from "../AuthenticationContext"
+import { useContext } from "react"
+import { useHistory } from "react-router-dom"
+
+const Header = () => {
+
+    const authContext = useContext(AuthenticationContext)
+    const history = useHistory() 
+
+    const logoutButton = () => {
+        authContext.logout()
+        history.push('/')
+    }
+
+    return (
+        <div>
+            { authContext.accountType && <button onClick={()=>{logoutButton()} } className="btn btn-primary"> Logout </button> }
+        </div>
+    )
+ 
+}
+
+export default Header
