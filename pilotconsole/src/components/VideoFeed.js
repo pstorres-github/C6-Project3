899cf355e5d651daeaaaecd585f527649d45184e
mKhan-11 <80430158+mKhan-11@users.noreply.github.com>
2021-07-06 02:18:51 -0600

    Video Feed now streams into Pilot Console - Haha :D

--- pilotconsole/src/components/VideoFeed.js
+++ pilotconsole/src/components/VideoFeed.js
@@ -1,14 +1,35 @@
-import React from "react"
+import React, { useEffect } from "react"
+import JMuxer from "jmuxer"
+
+const Buffer = require('buffer').Buffer
 
 const VideoFeed = ({ DroneVideoFeed }) => {
 
-    DroneVideoFeed.on('message', (videoDataStream) => {
-        console.log('Video Feed Stream : ', videoDataStream)
-    })
+    useEffect(() => {
+        let jmuxer = new JMuxer({
+            node: 'player',
+            mode: 'video',
+            fps: 30,
+            flushingTime: 1
+        })
+
+        const parseVideo = (dataStream) => {
+            jmuxer.feed({
+                video: Buffer.from(dataStream)
+            })
+        }
+
+        DroneVideoFeed.on('message', (videoDataStream) => {
+            parseVideo(videoDataStream)
+        })
+
+    }, [])
 
     return (
         <div>
-            Drone Video Feed
+            <div id="videoFeed">
+                <video autoPlay={true} id={"player"}></video>
+            </div>
         </div>
     )
 }
