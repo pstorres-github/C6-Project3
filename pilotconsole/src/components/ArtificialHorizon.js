0afc43eb946c2a0beb393028929830038c07a2fc
mKhan-11 <80430158+mKhan-11@users.noreply.github.com>
2021-07-10 14:03:00 -0600

    Artificial Horizon component added

---
+++ pilotconsole/src/components/ArtificialHorizon.js
@@ -0,0 +1,12 @@
+import React from "react"
+import TelemetryContext from "../TelemetryContext"
+import "./ArtificialHorizon.css"
+
+const ArtificialHorizon = () => {
+
+    return (
+        <div>Artifical Horizon</div>
+    )
+}
+
+export default ArtificialHorizon
