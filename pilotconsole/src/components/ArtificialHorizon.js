ec7d338c343e98472ba5b9d09c50acf4576d6d90
mKhan-11 <80430158+mKhan-11@users.noreply.github.com>
2021-07-14 19:58:15 -0600

    added telemetry context to AH component

--- pilotconsole/src/components/ArtificialHorizon.js
+++ pilotconsole/src/components/ArtificialHorizon.js
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { useContext } from "react"
 import TelemetryContext from "../TelemetryContext"
 import "./ArtificialHorizon.css"
 
 const ArtificialHorizon = () => {
+
+    // const telemetryContext = useContext(TelemetryContext)
 
     return (
         <div>Artifical Horizon</div>
