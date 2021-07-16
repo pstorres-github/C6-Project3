71eb4a873ec30b42fb12c4e7454a598d1e389859
mKhan-11 <80430158+mKhan-11@users.noreply.github.com>
2021-07-16 16:05:43 -0600

    added testing files and static frame for AH

--- pilotconsole/src/components/ArtificialHorizon.js
+++ pilotconsole/src/components/ArtificialHorizon.js
@@ -1,13 +1,36 @@
-import React, { useContext } from "react"
-import TelemetryContext from "../TelemetryContext"
 import "./ArtificialHorizon.css"
+
+import React, { useContext } from "react"
+
+import TelemetryContext from "../TelemetryContext"
+
+// import "./assets/HUD_static-frame.svg"
 
 const ArtificialHorizon = () => {
 
     // const telemetryContext = useContext(TelemetryContext)
 
     return (
-        <div>Artifical Horizon</div>
+        <div className="horizon">
+            <div className="static-frame">
+                <img src="./assets/HUD_static-frame.svg" alt="Artificial Horizon Base Frame" />
+            </div>
+           {/* <div className="heading-indicator">
+                <img src="./assets/HUD_heading-indicator.svg" alt="Heading Indicator" />
+            </div> */}
+            {/* <div className="pitch-indicator">
+                 <img src="./assets/HUD_pitch-indicator.svg" alt="Pitch Indicator" />
+             </div> */}
+            <div className="angle-of-bank-indicator">
+                <img src="./assets/HUD_angle-of-bank-indicator.svg" alt="Angle of Bank Indicator" />
+            </div>
+            {/* <div className="altitiude-text">
+                ALT
+            </div>
+            <div className="airspeed-text">
+                AIR
+            </div> */ }
+        </div>
     )
 }
 
