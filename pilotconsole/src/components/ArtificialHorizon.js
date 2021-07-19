21f840e32076015e233c610433242f365ca0f3f2
araadt <andy@vanderraadt.codes>
2021-07-19 07:31:32 -0600

    init artificial horizon

--- pilotconsole/src/components/ArtificialHorizon.js
+++ pilotconsole/src/components/ArtificialHorizon.js
@@ -1,35 +1,43 @@
-import "./ArtificialHorizon.css"
+import './ArtificialHorizon.css'
 
-import React, { useContext } from "react"
+import React, { useContext } from 'react'
 
-import TelemetryContext from "../TelemetryContext"
+import TelemetryContext from '../TelemetryContext'
 
 // import "./assets/HUD_static-frame.svg"
 
 const ArtificialHorizon = () => {
-
     // const telemetryContext = useContext(TelemetryContext)
+
+    const pitchPosition = {
+        backgroundPosition: 'calc(100%) calc(100% - 1890px)'
+    }
 
     return (
         <div className="horizon">
+            <div className="alt-text plex">ALT</div>
+            <div className="air-text plex">SPEED</div>
             <div className="static-frame">
-                <img src="./assets/HUD_static-frame.svg" alt="Artificial Horizon Base Frame" />
+                <img
+                    src="./assets/HUD_static-frame.svg"
+                    alt="Artificial Horizon Base Frame"
+                />
             </div>
-           {/* <div className="heading-indicator">
-                <img src="./assets/HUD_heading-indicator.svg" alt="Heading Indicator" />
+            {/* <div className="heading-indicator">
+                <img
+                    src="./assets/HUD_heading-indicator.svg"
+                    alt="Heading Indicator"
+                />
             </div> */}
-            {/* <div className="pitch-indicator">
-                 <img src="./assets/HUD_pitch-indicator.svg" alt="Pitch Indicator" />
-             </div> */}
+            <div className="pitch-indicator" style={pitchPosition}>
+                {/* spacer */}
+            </div>
             <div className="angle-of-bank-indicator">
-                <img src="./assets/HUD_angle-of-bank-indicator.svg" alt="Angle of Bank Indicator" />
+                <img
+                    src="./assets/HUD_angle-of-bank-indicator.svg"
+                    alt="Angle of Bank Indicator"
+                />
             </div>
-            {/* <div className="altitiude-text">
-                ALT
-            </div>
-            <div className="airspeed-text">
-                AIR
-            </div> */ }
         </div>
     )
 }
