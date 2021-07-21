838360fc343e85fffc6b5403f283f212a7879d29
araadt <andy@vanderraadt.codes>
2021-07-21 10:20:39 -0600

    move app-header to pilotConsole.js

--- pilotconsole/src/App.js
+++ pilotconsole/src/App.js
@@ -1,12 +1,17 @@
-import React, {useContext} from 'react'
-import { Switch, Route, BrowserRouter as Router, Redirect } from 'react-router-dom'
 import './App.css'
 
+import React, { useContext } from 'react'
+import {
+    Redirect,
+    Route,
+    BrowserRouter as Router,
+    Switch
+} from 'react-router-dom'
+
+import AuthenticationContext from './AuthenticationContext'
+import AuthenticationProvider from './AuthenticationProvider'
 import Homepage from './pages/Homepage'
 import PilotConsole from './pages/PilotConsole'
-import Header from './components/Header'
-import AuthenticationProvider from './AuthenticationProvider'
-import AuthenticationContext from "./AuthenticationContext"
 import TelemetryProvider from './TelemetryProvider'
 
 // Import Electron/Node Items
@@ -26,52 +31,53 @@ const DroneVideoFeed = dgram.createSocket('udp4')
 DroneVideoFeed.bind(11111)
 
 function App() {
+    return (
+        <TelemetryProvider>
+            <AuthenticationProvider>
+                <Router>
+                    <div className="app-content">
+                        <Switch>
+                            <Route exact path="/" render={() => <Homepage />} />
 
-  return (
-
-    <TelemetryProvider>
-      <AuthenticationProvider>
-
-        <Router>
-
-          <div className='app-header'>
-            <Header />
-          </div>
-
-          <div className='app-content'>
-            <Switch>
-              <Route exact path='/' render={() => (<Homepage />)} />
-
-              {/* Private path for pilot console.  If using guest account, logged in as guest */}
-              {/* <PrivateRoute exact path='/pilotconsole'> <PilotConsole DroneConnection={DroneConnection} DroneState={DroneState} DroneVideoFeed={DroneVideoFeed} /> </PrivateRoute> */}
-              <Route exact path='/pilotconsole' render={() => (<PilotConsole DroneConnection={DroneConnection} DroneState={DroneState} DroneVideoFeed={DroneVideoFeed} />)} />
-
-            </Switch>
-          </div>
-
-        </Router>
-      </AuthenticationProvider>
-    </TelemetryProvider>
-
-
-
-  )
+                            {/* Private path for pilot console.  If using guest account, logged in as guest */}
+                            {/* <PrivateRoute exact path='/pilotconsole'> <PilotConsole DroneConnection={DroneConnection} DroneState={DroneState} DroneVideoFeed={DroneVideoFeed} /> </PrivateRoute> */}
+                            <Route
+                                exact
+                                path="/pilotconsole"
+                                render={() => (
+                                    <PilotConsole
+                                        DroneConnection={DroneConnection}
+                                        DroneState={DroneState}
+                                        DroneVideoFeed={DroneVideoFeed}
+                                    />
+                                )}
+                            />
+                        </Switch>
+                    </div>
+                </Router>
+            </AuthenticationProvider>
+        </TelemetryProvider>
+    )
 }
 
 // route wrapper.  Only allow access to PrivateRoutes if user is logged in
 function PrivateRoute({ children, ...rest }) {
-
-  const authContext = useContext(AuthenticationContext)
-  console.log("authentication", authContext.email)
-  return (
-    <Route
-      {...rest}
-      render={({ location }) =>
-        authContext.email ? (children) : (<Redirect to={{ pathname: "/", state: { from: location } }} />)
-      }
-    />
-  );
+    const authContext = useContext(AuthenticationContext)
+    console.log('authentication', authContext.email)
+    return (
+        <Route
+            {...rest}
+            render={({ location }) =>
+                authContext.email ? (
+                    children
+                ) : (
+                    <Redirect
+                        to={{ pathname: '/', state: { from: location } }}
+                    />
+                )
+            }
+        />
+    )
 }
 
-
-export default App;
+export default App
