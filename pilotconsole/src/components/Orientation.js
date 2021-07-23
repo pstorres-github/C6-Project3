cc0c5a9616199c322a3f4ded86bb7477e6e6b54d
araadt <andy@vanderraadt.codes>
2021-07-22 19:58:24 -0600

    👨🏻‍🎨 layout changes

--- pilotconsole/src/components/Orientation.js
+++ pilotconsole/src/components/Orientation.js
@@ -1,64 +1,73 @@
-import React, { useState, useEffect, useContext } from "react"
-import TelemetryContext from "../TelemetryContext"
+import './Orientation.css'
+
+import React, { useContext, useEffect, useState } from 'react'
+
 import { Canvas } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import "./Orientation.css"
+import TelemetryContext from '../TelemetryContext'
 
 const Orientation = () => {
-
     const telemetryContext = useContext(TelemetryContext)
 
     return (
-      <div className="subsection-Orientation">
-        <Canvas>
-        <ambientLight intensity={0.5} />
-              <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-              <pointLight position={[-10, -10, -10]} />
-              <Drone rotation={[
-                      telemetryContext.pitch * Math.PI / 180,
-                      (telemetryContext.yaw-180) * Math.PI / 180, // so model is facing forward initially
-                      telemetryContext.roll * Math.PI / 180
-                    ]}
-                    position={[0, 0, 0]}
-                    scale={[.8, .8, .8]}
+        <div className="orientation-container">
+            <div className="subsection-orientation">
+                <Canvas>
+                    <ambientLight intensity={0.75} />
+                    <spotLight
+                        position={[20, 20, 20]}
+                        angle={0.15}
+                        penumbra={0}
                     />
-        </Canvas>
-        <table className="orientation-table">
-        <tr>
-            <th>Pitch (deg)</th>
-            <th>Yaw (deg)</th>
-            <th>Roll (deg)</th>
-            <th>Altitude (cm)</th>
-        </tr>      
-        <tr>
-            <td>{telemetryContext.pitch}</td>
-            <td>{telemetryContext.yaw}</td>
-            <td>{telemetryContext.roll}</td>
-            <td>{telemetryContext.altitude}</td>
-        </tr>   
-        </table>
-     </div>
-    )
-  }
-  
+                    <pointLight position={[-10, -10, -10]} />
+                    <Drone
+                        rotation={[
+                            (telemetryContext.pitch * Math.PI) / 180,
+                            ((telemetryContext.yaw - 180) * Math.PI) / 180, // so model is facing forward initially
+                            (telemetryContext.roll * Math.PI) / 180
+                        ]}
+                        position={[0, 0.25, -0.5]}
+                        scale={[0.75, 0.75, 0.75]}
+                    />
+                </Canvas>
 
-const Drone = ({position, rotation, scale}) => {
+                {/* temporary hide of telemetry table in CSS */}
+                <table className="orientation-table visually-hidden">
+                    <tr>
+                        <th>Pitch (deg)</th>
+                        <th>Yaw (deg)</th>
+                        <th>Roll (deg)</th>
+                        <th>Altitude (cm)</th>
+                    </tr>
+                    <tr>
+                        <td>{telemetryContext.pitch}</td>
+                        <td>{telemetryContext.yaw}</td>
+                        <td>{telemetryContext.roll}</td>
+                        <td>{telemetryContext.altitude}</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+const Drone = ({ position, rotation, scale }) => {
     const [model, setModel] = useState()
     const [arrow, setArrow] = useState()
-    
+
     useEffect(() => {
         new GLTFLoader().load('./assets/drone.glb', setModel)
-        new GLTFLoader().load('./assets/arrow.glb', setArrow)        
-    },[])
-    
+        new GLTFLoader().load('./assets/arrow.glb', setArrow)
+    }, [])
+
     if (!model) {
         return null
     }
-    
+
     return (
         <group position={position} rotation={rotation} scale={scale}>
-        <primitive object = {model.scene} />
-        <primitive object = {arrow.scene} />
+            <primitive object={model.scene} />
+            <primitive object={arrow.scene} />
         </group>
     )
 }
