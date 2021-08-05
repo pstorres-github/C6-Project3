fe3bdcac9577c2230990938f7c86e3c2a8f96cce
araadt <andy@vanderraadt.codes>
2021-08-05 14:06:34 -0600

    minor revisions

--- dashboard/src/components/AccountTypeIcons.js
+++ dashboard/src/components/AccountTypeIcons.js
@@ -1,35 +1,52 @@
 import './AccountTypeIcons.css'
 
+import {
+    faCertificate,
+    faIdCardAlt,
+    faUserAlt
+} from '@fortawesome/free-solid-svg-icons'
+
 import AuthenticationContext from '../AuthenticationContext'
-import { useContext } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faIdCardAlt, faCertificate, faUserAlt } from '@fortawesome/free-solid-svg-icons'
+import { useContext } from 'react'
 
 const AccountTypeIcons = () => {
-  const authContext = useContext(AuthenticationContext)
+    const authContext = useContext(AuthenticationContext)
 
-  let icon
-  // console.log(`account type`, authContext.accountType)
-  
-  if (authContext.accountType == 'admin') {
-      // console.log('admin-icon') 
-      // icon = 'admin-icon' 
-      icon = <FontAwesomeIcon className="fontawesome-icon-correction" icon={faCertificate} /> 
-      return icon
-  } else if (authContext.accountType == 'customer') {
-      // console.log('customer-icon') 
-      // icon = 'customer-icon'
-      icon = <FontAwesomeIcon  className="fontawesome-icon-correction" icon={faUserAlt} /> 
-      return icon
-  } else if (authContext.accountType == 'pilot') {
-      // console.log('pilot-icon') 
-      // icon = 'pilot-icon'
-      icon = <FontAwesomeIcon  className="fontawesome-icon-correction" icon={faIdCardAlt} /> 
+    let icon
+    // console.log(`account type`, authContext.accountType)
 
-      return icon
-  } else {
-      return icon
-  }
+    if (authContext.accountType == 'admin') {
+        // console.log('admin-icon')
+        // icon = 'admin-icon'
+        icon = (
+            <span className="fontawesome-icon-admin">
+                <FontAwesomeIcon icon={faCertificate} className="icon" />
+            </span>
+        )
+        return icon
+    } else if (authContext.accountType == 'customer') {
+        // console.log('customer-icon')
+        // icon = 'customer-icon'
+        icon = (
+            <span className="fontawesome-icon-customer">
+                <FontAwesomeIcon icon={faUserAlt} />
+            </span>
+        )
+        return icon
+    } else if (authContext.accountType == 'pilot') {
+        // console.log('pilot-icon')
+        // icon = 'pilot-icon'
+        icon = (
+            <span className="fontawesome-icon-pilot">
+                <FontAwesomeIcon icon={faIdCardAlt} />
+            </span>
+        )
+
+        return icon
+    } else {
+        return icon
+    }
 }
 
 export default AccountTypeIcons
