6c34075901601ffbef64be380bf3f870b765c825
PTorres <pstorres@gmail.com>
2021-08-16 14:57:24 -0600

    re-activated the background cycler

--- dashboard/src/pages/Homepage.js
+++ dashboard/src/pages/Homepage.js
@@ -1,86 +1,110 @@
 import './Homepage.css'
 
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 import Login from '../components/Login'
 import Register from '../components/Register'
+import bg1 from '../assets/backgrounds/1.png'
+import bg2 from '../assets/backgrounds/6.png'
+import bg3 from '../assets/backgrounds/5.png'
 
 const Homepage = () => {
     const [viewMode, setViewMode] = useState('logIn')
+    const bgChange = useRef()
 
-    // VDR: Ignore this for now
-    // function bgImgScheduler1() {
-    //     setTimeout(() => {
-    //         document.querySelector('.img1').style.opacity = 0
-    //         document.querySelector('.img2').style.opacity = 1
-    //         document.querySelector('.img3').style.opacity = 1
-    //         order(
-    //             ['-3', '-1', '-2'],
-    //             () => {
-    //                 bgImgScheduler2()
-    //             },
-    //             1000
-    //         )
-    //     }, 3000)
-    // }
+    useEffect (()=>{
+            let bgChange = true
 
-    // function bgImgScheduler2() {
-    //     setTimeout(() => {
-    //         document.querySelector('.img1').style.opacity = 1
-    //         document.querySelector('.img2').style.opacity = 0
-    //         document.querySelector('.img3').style.opacity = 1
-    //         order(
-    //             ['-2', '-3', '-1'],
-    //             () => {
-    //                 bgImgScheduler3()
-    //             },
-    //             1000
-    //         )
-    //     }, 3000)
-    // }
+            function bgImgScheduler1() {
 
-    // function bgImgScheduler3() {
-    //     setTimeout(() => {
-    //         document.querySelector('.img1').style.opacity = 1
-    //         document.querySelector('.img2').style.opacity = 1
-    //         document.querySelector('.img3').style.opacity = 0
-    //         order(
-    //             ['-1', '-2', '-3'],
-    //             () => {
-    //                 bgImgScheduler1()
-    //             },
-    //             1000
-    //         )
-    //     }, 3000)
-    // }
+                setTimeout(() => {
+                    if (bgChange === true) {
+                        document.querySelector('.img1').style.opacity = 0
+                        document.querySelector('.img2').style.opacity = 1
+                        document.querySelector('.img3').style.opacity = 1
+                    }
+                    order(
+                        ['-3', '-1', '-2'],
+                        () => {
+                            bgImgScheduler2()
+                        },
+                        1000
+                    )
+                }, 3000)
+            }
 
-    // function order(array, callback, time) {
-    //     setTimeout(() => {
-    //         document.querySelector('.img1').style.zIndex = array[0]
-    //         document.querySelector('.img2').style.zIndex = array[1]
-    //         document.querySelector('.img3').style.zIndex = array[2]
-    //         callback()
-    //     }, time)
-    // }
+            function bgImgScheduler2() {
+            
+                    setTimeout(() => {
+                        if (bgChange === true) {
+                        document.querySelector('.img1').style.opacity = 1
+                        document.querySelector('.img2').style.opacity = 0
+                        document.querySelector('.img3').style.opacity = 1
+                        }
+                        order(
+                            ['-2', '-3', '-1'],
+                            () => {
+                                bgImgScheduler3()
+                            },
+                            1000
+                        )
+                    }, 3000)
+            }
 
-    // bgImgScheduler1()
+            function bgImgScheduler3() {
+                    setTimeout(() => {
+                        if (bgChange === true) {
+                            document.querySelector('.img1').style.opacity = 1
+                            document.querySelector('.img2').style.opacity = 1
+                            document.querySelector('.img3').style.opacity = 0
+                        }
+                        order(
+                            ['-1', '-2', '-3'],
+                            () => {
+                                bgImgScheduler1()
+                            },
+                            1000
+                        )
+                    }, 3000)
+            }
+
+            function order(array, callback, time) {
+                if (bgChange ===true)
+                    setTimeout(() => {
+                        if (document.querySelector('.img1') && document.querySelector('.img2') && document.querySelector('.img3'))
+                        document.querySelector('.img1').style.zIndex = array[0]
+                        document.querySelector('.img2').style.zIndex = array[1]
+                        document.querySelector('.img3').style.zIndex = array[2]
+                        callback()
+                    }, time)
+            }
+
+        bgImgScheduler1()
+
+        return () => {
+            bgChange = false
+        }
+    },[bgChange])
 
     return (
         <div className="grid-container">
             {/* VDR: Ignore this for now */}
             {/* background images */}
-            {/* <img
+            <img
                 class="background-image img1"
-                // src="../assets/backgrounds/1.png"
+                src={bg1}
+                alt=""
             ></img>
             <img
                 class="background-image img2"
-                // src="../assets/backgrounds/2.png"
+                src={bg2}
+                alt=""
             ></img>
             <img
                 class="background-image img3"
-                // src="../assets/backgrounds/3.png"
-            ></img> */}
+                src={bg3}
+                alt=""
+            ></img>
 
             <div className="header"></div>
 
