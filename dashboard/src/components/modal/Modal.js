4cfcc74b51a3f6c4a22cdc691f244d2defaca4f7
rhaissamrv <mrvrhai@gmail.com>
2021-08-23 17:57:23 -0600

    edited waypoint in WorkOderDetails to show lat and lng pairs per line

--- dashboard/src/components/modal/Modal.js
+++ dashboard/src/components/modal/Modal.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useRef } from 'react';
 // import Button from '../button/Button';
 // import CloseIcon from '../CloseIcon';
+import {
+    faTimes
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import styles from './modal.module.css';
 
 const Modal = ({ modalStyle, children, show, onClose, backdropStyle }) => {
@@ -21,14 +26,21 @@ const Modal = ({ modalStyle, children, show, onClose, backdropStyle }) => {
     return (
         <React.Fragment>
             <div ref={modalRef} style={backdropStyle} className={`${styles.modal__wrap}`}>
-                <img
+                <div>
+                {/* <img
                     alt="close-button"
                     src="close-icon.png"
                     onClick={onClose}
                     style={{ width: 40, height: 40, position: 'absolute', top: 0, right: 0, margin: '1rem' }}
-                ></img>
+                ></img> */}
+                <div className='full-width'>
+                <button onClick={onClose}>
+                <FontAwesomeIcon icon={faTimes}  />
+                </button>
+                </div>
                 <div style={modalStyle} className={styles.modal}>
                     {children}
+                </div>
                 </div>
             </div>
         </React.Fragment>
